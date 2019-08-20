8c08d3268387fb8b297de7eac0f19facd518fc6a
toohong5 <toohong5@gmail.com>
2019-08-21 01:22:23 +0900

    190820 | 보충

--- 보충/bomber1.py
+++ 보충/bomber1.py
@@ -6,9 +6,54 @@ for tc in range(1, T + 1):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
     
+    row = []
+    col = []
     for i in range(n):
-        sum = 0
+        row_sum = 0
+        col_sum = 0
         for j in range(n):
             row_sum += arr[i][j]
             col_sum += arr[j][i]
-            sum = sum + row_sum + col_sum - arr[]
+        row.append(row_sum)
+        col.append(col_sum)
+
+    max_sum = 0
+    r = c = 0
+    for x in range(n):
+        for y in range(n):
+            total = 0
+            total = row[x] + col[y] - arr[x][y]
+            if max_sum < total:
+                max_sum = total
+                r = x
+                c = y
+            elif max_sum == total:
+                r = x
+                c = y
+    
+    print('#{} {} {} {}'.format(tc, r, c, max_sum))
+
+
+
+
+    # max_sum = 0
+    # r=0
+    # c=0
+    # for i in range(n):
+    #     row_sum = 0
+    #     col_sum = 0
+    #     for j in range(n):
+    #         sum = 0
+    #         sub = 0
+    #         row_sum += arr[i][j]
+    #         for k in range(n):
+    #             col_sum += arr[k][j]
+    #             if arr[i][j] == arr[k][j]:
+    #                 sub = arr[i][j]
+    #         sum = sum + row_sum + col_sum - sub
+    #         if sum > max_sum:
+    #             max_sum = sum
+    #             r = i
+    #             c = j
+    # print(sum)
+
---
+++ 보충/bomber2.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open('input1.txt', 'r')
+
+T = int(input())
+for tc in range(1, T + 1):
+    n = int(input())
+
+    
