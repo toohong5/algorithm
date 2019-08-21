c096d053f8587447f5b7eeea985a9d5e756441ae
toohong5 <toohong5@gmail.com>
2019-08-22 02:05:29 +0900

    190821 | 보충

--- 190806/2일차_색종이.py
+++ 190806/2일차_색종이.py
@@ -1,10 +1,22 @@
 import sys
-sys.stdin('input2_7.txt', 'r')
+sys.stdin = open('input2_8.txt', 'r')
 
 n = int(input())
-for tc in range(1, n+1):
-    x, y, dx, dy = map(int, input().split())
-    x2, y2, dx2, dy2 = map(int, input().split())
+arr = [[0] * 101 for _ in range(101)]
+for i in range(1, n + 1):
+    count = 0
+    box = list(map(int, input().split()))
+    for p in range(box[0], box[2] + 1):
+        for q in range(box[1], box[3] + 1):
+            if arr[p][q] == 0:
+                arr[p][q] = i
+            elif arr[p][q] != 0 and arr[p][q] != i:
+                arr[p][q] = i
+    else:
+        for k in range(101):
+            count += arr[k].count(i)
+    print(count)
 
-    box1 = [[0] * dx for i in range(dx)]
+    # print(arr[i-1].count(i))
+
     
